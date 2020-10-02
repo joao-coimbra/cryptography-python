42ddfb577f16c2e4b08f36e656c0b99563e35c68
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-10-01 22:35:09 -0300

    config the Vigenère cipher

--- cifraVigenere.py
+++ cifraVigenere.py
@@ -1,47 +1,9 @@
+# imports
+from utilities import removeSpecialCharacters
+from cryptography import vigenere
+
 # Title
 print('\n\tCifra de Vigenère\n')
-
-# vars
-# alphabet list
-alphabet = list(map(chr, range(97, 123)))
-
-# use in remove special characters
-x = ''
-
-# functions
-def phrase(x):
-    phrase = input(f'\nDigite a frase que deseja {x}\n-> ').lower()
-    return phrase
-
-def encrypt(x, n, result = ''):
-    # C = P + K (mod 26)
-    for c in x:
-        if c in alphabet:
-            i = alphabet.index(c)
-            if i+n > 25:
-                i = i - 26 + n
-                result += alphabet[i]
-            else:
-                result += alphabet[i+n]
-        else:
-            result += c
-
-    return result.upper()
-
-def decrypt(x, n, result = ''):]
-    # P = C - K + 26 (mod 26)
-    for c in x:
-        if c in alphabet:
-            i = alphabet.index(c)
-            if i-n < 0:
-                i = i + 26 - n
-                result += alphabet[i]
-            else:
-                result += alphabet[i-n]
-        else:
-            result += c
-
-    return result
 
 # data requirement
 while True:
@@ -56,34 +18,42 @@ while True:
     else:
         print('\nEscolha inválida\n')
 
+# define phrase
+phrase = input(f'\nDigite a frase que deseja (não coloque espaços nem números) {choice}\n-> ').lower()
+
+# define keyword
+error = False
 while True:
-    try:
-        n = int(input('\nEscolha o parâmetro que será utilizado [1-25]:\n-> '))
-        if 1 <= n <= 25:
+    keyword = input(f'\nDigite a palavra-chave que deseja (não coloque espaços nem números)\n-> ').lower()
+    
+    for i in range(len(keyword)):
+        try:
+            int(keyword[i])
+
+            error = True
+            print('Digite uma palavra-chave sem espaços ou números.')
             break
-        else:
-            print('Digite um parâmetro entre 1 e 20')
-            continue
-    except ValueError:
-        print('Digite um valor inteiro')
+        except:
+            if keyword[i] == ' ':
+                print('Digite uma palavra-chave sem espaços ou números.')
+
+                error = True
+                break
+            else:
+                pass
+        error = False
+
+    if not error:
+        break
 
 # remove special characters
-for c in phrase(choice):
-    if c == 'ã' or c == 'á' or c == 'à' or c == 'â' or c == 'ä':
-        x += 'a'
-    elif c == 'é' or c == 'è' or c == 'ê' or c == 'ë':
-        x += 'e'
-    elif c == 'í' or c == 'î' or c == 'ï' or c == 'ì':
-        x += 'i'
-    elif c == 'ó' or c == 'ò' or c == 'ô' or c == 'ö':
-        x += 'o'
-    elif c == 'ú' or c == 'ù' or c == 'ü':
-        x += 'u'
-    else:
-        x += c
+phrase = removeSpecialCharacters(phrase)
+keyword = removeSpecialCharacters(keyword)
 
 # script
 if choice == 'criptografar':
-    print(f'\n{encrypt(x, n)}\n')
+    typeEncrypt = 'encrypt'
+    print(f'\n{vigenere(phrase, keyword, typeEncrypt)}\n')
 else:
-    print(f'\n{decrypt(x, n)}\n')
+    typeEncrypt = 'decrypt'
+    print(f'\n{vigenere(phrase, keyword, typeEncrypt)}\n')
--- cryptography.py
+++ cryptography.py
@@ -1,35 +1,95 @@
 # define alphabet list
 alphabet = list(map(chr, range(97, 123)))
 
-def cesar(x, n, typeEncrypt, result = ''):
+# crypt césar
+def cesar(phrase, n, typeEncrypt, result = ''):
 
     if typeEncrypt == 'encrypt':
 
-        for c in x:
-            if c in alphabet:
-                i = alphabet.index(c)
-                if i+n > 25:
-                    i = i - 26 + n
-                    result += alphabet[i]
-                else:
-                    result += alphabet[i+n]
+        for letter in phrase:
+            if letter in alphabet:
+                p = alphabet.index(letter)
+                c = p + n
+                if c > 25:
+                    c -= 26
+
+                result += alphabet[c]
+
             else:
-                result += c
+                result += letter
 
         result = result.upper()
 
     elif typeEncrypt == 'decrypt':
 
-        for c in x:
-            if c in alphabet:
-                i = alphabet.index(c)
-                if i-n < 0:
-                    i = i + 26 - n
-                    result += alphabet[i]
-                else:
-                    result += alphabet[i-n]
+        for letter in phrase:
+            if letter in alphabet:
+                c = alphabet.index(letter)
+                p = c - n
+                if p < 0:
+                    p += 26
+
+                result += alphabet[p]
+
             else:
-                result += c
+                result += letter
+
+    else:
+        return 'Error typeEncrypt'
+
+    return result
+
+# crypt vigenère
+def vigenere(phrase, keyword, typeEncrypt, n = 1, z = 0, newPhrase = '', result = ''):
+
+    for x in range(len(phrase)):
+        if phrase[x] in alphabet:
+            if z >= len(keyword) * n:
+                i = z - len(keyword) * n
+                try:
+                    newPhrase += keyword[i]
+                except IndexError:
+                    n += 1
+                    i = z - len(keyword) * n
+                    newPhrase += keyword[i]
+            else:
+                newPhrase += keyword[z]
+            
+            z += 1
+        else:
+            newPhrase += phrase[x]
+
+    if typeEncrypt == 'encrypt':
+        for x in range(len(phrase)):
+            if phrase[x] in alphabet:
+                p = alphabet.index(phrase[x])
+                k = alphabet.index(newPhrase[x])
+                c = p + k
+
+                if c > 25:
+                    c -= 26
+
+                result += alphabet[c]
+
+            else:
+                result += phrase[x]
+
+        result = result.upper()
+    
+    elif typeEncrypt == 'decrypt':
+        for x in range(len(phrase)):
+            if phrase[x] in alphabet:
+                c = alphabet.index(phrase[x])
+                k = alphabet.index(newPhrase[x])
+                p = c - k
+
+                if p < 0:
+                    p += 26
+
+                result += alphabet[p]
+
+            else:
+                result += phrase[x]
 
     else:
         return 'Error typeEncrypt'
