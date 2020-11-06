58e7e2fa083b16e6dabd8e0b57c8239b6882463e
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-11-05 23:10:01 -0300

    created CipherMachine class
    created CipherMachine class
    organized files
    organized system and operations
    fixed bugs

--- cryptography.py
+++ cryptography.py
@@ -1,97 +1,172 @@
-# define alphabet list
-alphabet = list(map(chr, range(97, 123)))
+import utilities as ut
 
-# crypt césar
-def cesar(phrase, n, typeEncrypt, result = ''):
+class CipherMachine:
 
-    if typeEncrypt == 'encrypt':
+  def __init__(self):
+    self.encryptType = self.defineEncryptType()
+    self.phrase = self.defineText()
 
-        for letter in phrase:
-            if letter in alphabet:
-                p = alphabet.index(letter)
-                c = p + n
-                if c > 25:
-                    c -= 26
+  def defineText(self):
+    # define phrase
+    phrase = input(f'\nDigite a frase que deseja {self.encryptType}\n-> ').lower()
 
-                result += alphabet[c]
+    # remove special characters
+    phrase = ut.removeSpecialCharacters(phrase)
 
-            else:
-                result += letter
+    return phrase
 
-        result = result.upper()
+  def defineEncryptType(self):
+    while True:
+      choice = input('Deseja [c]criptografar ou [d]descriptografar?\n-> ')
 
-    elif typeEncrypt == 'decrypt':
+      if choice == 'c' or choice == 'd':
+          if choice == 'c':
+              choice = 'criptografar'
+          else:
+              choice = 'descriptografar'
+          break
+      else:
+          print('\nEscolha inválida\n')
 
-        for letter in phrase:
-            if letter in alphabet:
-                c = alphabet.index(letter)
-                p = c - n
-                if p < 0:
-                    p += 26
+    return choice
 
-                result += alphabet[p]
+  # césar cipher
+  def cesar(self):
 
-            else:
-                result += letter
+    def parameter():
+      while True:
+        try:
+          parameter = int(input('\nEscolha o parâmetro que será utilizado [1-25]:\n-> '))
+          if 1 <= parameter <= 25:
+            break
+          else:
+            print('Digite um parâmetro entre 1 e 20')
+            continue
+        except ValueError:
+          print('Digite um valor inteiro')
+      return parameter
 
-    else:
-        return 'Error typeEncrypt'
+    def encrypt(parameter, result = ''):
 
-    return result
+      for letter in self.phrase:
+        if letter in ut.alphabet:
+          p = ut.alphabet.index(letter)
+          c = p + parameter
+          if c > 25:
+            c -= 26
 
-# crypt vigenère
-def vigenere(phrase, keyword, typeEncrypt, n = 1, z = 0, newPhrase = '', result = ''):
+          result += ut.alphabet[c]
 
-    for x in range(len(phrase)):
-        if phrase[x] in alphabet:
-            if z >= len(keyword) * n:
-                i = z - len(keyword) * n
-                try:
-                    newPhrase += keyword[i]
-                except IndexError:
-                    n += 1
-                    i = z - len(keyword) * n
-                    newPhrase += keyword[i]
-            else:
-                newPhrase += keyword[z]
-            
-            z += 1
         else:
-            newPhrase += phrase[x]
+          result += letter
 
-    if typeEncrypt == 'encrypt':
-        for x in range(len(phrase)):
-            if phrase[x] in alphabet:
-                p = alphabet.index(phrase[x])
-                k = alphabet.index(newPhrase[x])
-                c = p + k
+      return result.upper()
 
-                if c > 25:
-                    c -= 26
+    def decrypt(parameter, result = ''):
 
-                result += alphabet[c]
+      for letter in self.phrase:
+        if letter in ut.alphabet:
+          c = ut.alphabet.index(letter)
+          p = c - parameter
+          if p < 0:
+            p += 26
 
-            else:
-                result += phrase[x]
+          result += ut.alphabet[p]
 
-        result = result.upper()
+        else:
+          result += letter
+
+      return result
+
+    cryptography = lambda encryptType: encrypt(parameter()) if (encryptType == 'criptografar') else decrypt(parameter())
+    return cryptography(self.encryptType)
+
+  # vigenère cipher
+  def vigenere(self):
+
+    def keyword(error = False):
+      while True:
+        keyword = input(f'\nDigite a palavra-chave que deseja\n[não coloque espaços nem números]\n-> ').lower()
     
-    elif typeEncrypt == 'decrypt':
-        for x in range(len(phrase)):
-            if phrase[x] in alphabet:
-                c = alphabet.index(phrase[x])
-                k = alphabet.index(newPhrase[x])
-                p = c - k
+        for i in range(len(keyword)):
+          try:
+            int(keyword[i])
 
-                if p < 0:
-                    p += 26
+            error = True
+            print('Digite uma palavra-chave sem espaços ou números.')
+            break
+          except:
+            if keyword[i] == chr(32):
+              print('Digite uma palavra-chave sem espaços ou números.')
 
-                result += alphabet[p]
-
+              error = True
+              break
             else:
-                result += phrase[x]
+              pass
+          error = False
 
-    else:
-        return 'Error typeEncrypt'
+        if not error:
+          break
 
-    return result
+      return ut.removeSpecialCharacters(keyword)
+
+    def newPhrase(keyword = keyword(), n = 1, z = 0, newPhrase = ''):
+      for x in range(len(self.phrase)):
+        if self.phrase[x] in ut.alphabet:
+          if z >= len(keyword) * n:
+            i = z - len(keyword) * n
+            try:
+              newPhrase += keyword[i]
+            except IndexError:
+              n += 1
+              i = z - len(keyword) * n
+              newPhrase += keyword[i]
+          else:
+            newPhrase += keyword[z]
+            
+          z += 1
+        else:
+          newPhrase += self.phrase[x]
+
+      return newPhrase
+
+    def encrypt(result = ''):
+
+      for x in range(len(self.phrase)):
+        if self.phrase[x] in ut.alphabet:
+          p = ut.alphabet.index(self.phrase[x])
+          k = ut.alphabet.index(newPhrase[x])
+          c = p + k
+
+          if c > 25:
+            c -= 26
+
+          result += ut.alphabet[c]
+
+        else:
+          result += self.phrase[x]
+
+      return result.upper()
+
+    def decrypt(result = ''):
+
+      for x in range(len(self.phrase)):
+        if self.phrase[x] in ut.alphabet:
+          c = ut.alphabet.index(self.phrase[x])
+          k = ut.alphabet.index(newPhrase[x])
+          p = c - k
+
+          if p < 0:
+            p += 26
+
+          result += ut.alphabet[p]
+
+        else:
+          result += self.phrase[x]
+
+      return result
+
+    newPhrase = newPhrase()
+
+    cryptography = lambda encryptType: (encrypt(), decrypt())[encryptType == 'descriptografar']
+    return cryptography(self.encryptType)
